07fba77509ee43787f81930bea9a39ddbc9458b6
tarun5858 <ts460000@gmail.com>
2025-10-16 14:04:08 +0530

    latest commit

--- vite.config.js
+++ vite.config.js
@@ -41,17 +41,17 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { copyFileSync } from 'fs'
+// import { resolve } from 'path'
+// import { copyFileSync } from 'fs'
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-redirects',
-      writeBundle() {
-        copyFileSync(resolve('public/_redirects'), resolve('dist/_redirects'))
-      }
-    }
+    // {
+    //   name: 'copy-redirects',
+    //   writeBundle() {
+    //     copyFileSync(resolve('public/_redirects'), resolve('dist/_redirects'))
+    //   }
+    // }
   ]
 })
 
