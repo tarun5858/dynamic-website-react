34d45b08f8c118441df2a96eb5af5958f136c0b9
tarun5858 <ts460000@gmail.com>
2025-11-03 12:14:05 +0530

    latest commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import { createRoot,ReactDOM  } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+// Global CSS files
 import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // This is fine
+
+// App components
+import App from './App.jsx'
 import AnalyticsTracker from './components/AnalyticsTracker.jsx';
 import MuiRootProvider from './components/MuiRootProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-    <BrowserRouter>
-   <AnalyticsTracker />
-   <MuiRootProvider>
-
-        <App />
-   </MuiRootProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* MuiRootProvider MUST wrap the Router */}
+        <MuiRootProvider> 
+            <BrowserRouter>
+                {/* AnalyticsTracker remains inside the Router (needs useLocation) */}
+                <AnalyticsTracker />
+                <App />
+            </BrowserRouter>
+        </MuiRootProvider>
+    </React.StrictMode>
 )
