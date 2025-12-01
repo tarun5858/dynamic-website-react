7e6712c40269aca16115bd25fb42b51f15a208a6
tarun5858 <ts460000@gmail.com>
2025-12-01 11:39:56 +0530

    latest commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 
 //  STEP 1: MUI/EMOTION PROVIDER IMPORTS (Must be first)
 import MuiRootProvider from './components/MuiRootProvider.jsx';
-
+import ReactDOM from 'react-dom/client'
 // STEP 2: APPLICATION IMPORTS 
 import App from './App.jsx'
 // import AnalyticsTracker from './components/AnalyticsTracker.jsx';
@@ -12,9 +12,12 @@ import App from './App.jsx'
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Keep the JS bundle
 
 import ReactGA from "react-ga4";
+
+
 ReactGA.initialize("G-J2LQXQ630G");
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* MuiRootProvider (Emotion Cache/Theme) must wrap everything */}
         <MuiRootProvider> 
