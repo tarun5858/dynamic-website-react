0fa5f9c852b151a80378f3eba522a39c70aeea74
tarun5858 <ts460000@gmail.com>
2025-12-02 10:32:28 +0530

    latest commit

--- src/components/LargeCtaRoutes.jsx
+++ src/components/LargeCtaRoutes.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+// import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 const LargeCtaRoutes = ({ text, to, style }) => {
 
-    const handleCtaRoutes = () => {
-    // 1. Send the data to Google Analytics
-    ReactGA.event({
-      category: "User Interaction", // Broad category
-      action: "Clicked CtaRoutes",    // Specific action
-      label: "Homepage Hero",       // Where it happened (optional)
-      value: 1                      // Value associated with event (optional)
-    });
+  //   const handleCtaRoutes = () => {
+  //   // 1. Send the data to Google Analytics
+  //   ReactGA.event({
+  //     category: "User Interaction", // Broad category
+  //     action: "Clicked CtaRoutes",    // Specific action
+  //     label: "Homepage Hero",       // Where it happened (optional)
+  //     value: 1                      // Value associated with event (optional)
+  //   });
 
-    // 2. Perform actual logic (redirect, submit, etc.)
-    console.log("User Clicked CtaRoutes");
-  };
+  //   // 2. Perform actual logic (redirect, submit, etc.)
+  //   console.log("User Clicked CtaRoutes");
+  // };
 
 
   return (
@@ -25,7 +25,7 @@ const LargeCtaRoutes = ({ text, to, style }) => {
       data-delay-in="0.9"
       to={to}
       style={style}
-      onClick={handleCtaRoutes}
+      // onClick={handleCtaRoutes}
     >
       {text}
     </Link>
